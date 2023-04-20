e3c8a40c563ab454160eba0af39513f2cbc3f922
Anderson <andersonmendes05@gmail.com>
2023-04-20 11:30:25 -0300

    Conclusão TODO-APP

---
+++ 7_todo_app/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import pageHeader from '../template/pageHeader'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut voluptates molestias rerum illo. Doloribus facilis perferendis aut sunt dolores quia amet voluptates magni inventore exercitationem, dolorem nihil itaque veritatis, minima deleniti vitae sit similique illum ab sequi, magnam tempore repellat? Exercitationem autem consectetur velit quam ducimus natus. Necessitatibus explicabo voluptate dolorum soluta dolore officia, perspiciatis, in laborum, beatae nesciunt ipsam? Fugit blanditiis quos necessitatibus excepturi dolor placeat, magnam quae incidunt neque voluptatem! Numquam omnis maxime ipsa earum ducimus adipisci excepturi temporibus aliquid accusamus. Adipisci quis, consequuntur dolorem fugiat quam quibusdam nisi perferendis necessitatibus at molestias, vel quia a tenetur. Nihil.</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, nemo provident deserunt in veritatis tempore non quae quas, hic consectetur similique, modi iure temporibus adipisci perferendis nam. Totam consequuntur eius est necessitatibus officia consectetur consequatur! Eos debitis atque, iste magnam quas, ducimus molestiae voluptatibus error quod nesciunt, amet ipsa architecto?</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus minima ut quam incidunt aliquid labore, quasi explicabo deserunt, vel doloremque adipisci illum nisi, reprehenderit fuga tenetur maxime fugit. Pariatur, ducimus.</p>
+  </div>
+
+)
