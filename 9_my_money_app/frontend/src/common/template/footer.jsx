c6a0100fa30b4175527c8be8964a77a59cf63c13
Anderson <andersonmendes05@gmail.com>
2023-05-23 17:15:52 -0300

    adicionadas as funcionalidades do Dashboad

---
+++ 9_my_money_app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2023
+      <a href="https://github.com/andersonalbuqq" target="_blank"> Anderson</a>.
+    </strong>
+  </footer>
+)
