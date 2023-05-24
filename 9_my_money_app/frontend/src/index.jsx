218e579b94f0a7a36bc9fd7ff7048915f290084a
Anderson <andersonmendes05@gmail.com>
2023-05-24 16:12:36 -0300

    Inicio do ciclo de pagamentos

--- 9_my_money_app/frontend/src/index.jsx
+++ 9_my_money_app/frontend/src/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from "react-dom"
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
-import App from './main/app'
-import reducers from './main/reducers'
+import promise from "redux-promise";
 
-const store = createStore(reducers)
+import App from "./main/app";
+import reducers from "./main/reducers";
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-, document.getElementById('app'))
+  </Provider>,
+  document.getElementById("app")
+);
