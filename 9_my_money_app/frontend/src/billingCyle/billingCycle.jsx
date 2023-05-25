bf3d0a55d9417635cee1f9c4d256f580d939f182
Anderson <andersonmendes05@gmail.com>
2023-05-25 16:12:37 -0300

    Adicionada o formulario de inserção de dados

--- 9_my_money_app/frontend/src/billingCyle/billingCycle.jsx
+++ 9_my_money_app/frontend/src/billingCyle/billingCycle.jsx
@@ -10,12 +10,15 @@ import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 import { selectTab, showTabs } from "../common/tab/tabActions";
-import List from './billingCycleList'
+import { create } from "./billingCycleActions";
+
+import List from "./billingCycleList";
+import Form from "./billingCycleForm";
 
 class BillingCycle extends Component {
   componentWillMount() {
     this.props.selectTab("tabList");
-    this.props.showTabs('tabList', 'tabCreate')
+    this.props.showTabs("tabList", "tabCreate");
   }
 
   render() {
@@ -35,7 +38,7 @@ class BillingCycle extends Component {
                 <List />
               </TabContent>
               <TabContent id="tabCreate">
-                <h1>Incluir</h1>
+                <Form onSubmit={this.props.create}/>
               </TabContent>
               <TabContent id="tabUpdate">
                 <h1>Alterar</h1>
@@ -52,5 +55,5 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ selectTab, showTabs }, dispatch);
+  bindActionCreators({ selectTab, showTabs, create }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle);
